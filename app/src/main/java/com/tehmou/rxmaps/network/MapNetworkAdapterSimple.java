ea080362673b8eeac6a11fba76e931fe8f825fa9
Timo Tuominen <timo.tuominen@gmail.com>
2014-09-02 17:10:51 +0400

    Add temporary solution of reading tiles from disk

--- app/src/main/java/com/tehmou/rxmaps/network/MapNetworkAdapterSimple.java
+++ app/src/main/java/com/tehmou/rxmaps/network/MapNetworkAdapterSimple.java
@@ -1,10 +1,12 @@
 package com.tehmou.rxmaps.network;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import com.tehmou.rxmaps.Configuration;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -29,9 +31,34 @@ public class MapNetworkAdapterSimple implements MapNetworkAdapter {
     public Observable<Bitmap> getMapTile(final int zoom, final int x, final int y) {
         Log.d(TAG, "getMapTile(" + zoom + ", " + x + ", " + y + ")");
         final String url = String.format(urlFormat, zoom, x, y);
-        return networkClient
-                .loadBitmap(url);
-                //.doOnNext(writeOnDisk(zoom, x, y));
+
+        return Observable.from(readFromDisk(zoom, x, y));
+        //return networkClient
+        //        .loadBitmap(url);
+        //        .doOnNext(writeOnDisk(zoom, x, y));
+    }
+
+    static private String getFilename(final int zoom, final int x, final int y) {
+        return "/data/data/com.tehmou.rxmaps/" + zoom + "_" + x + "_" + y + ".png";
+    }
+
+    static private Bitmap readFromDisk(final int zoom, final int x, final int y) {
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(getFilename(zoom, x, y));
+            return BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
     static private Action1<Bitmap> writeOnDisk(final int zoom, final int x, final int y) {
@@ -40,7 +67,7 @@ public class MapNetworkAdapterSimple implements MapNetworkAdapter {
             public void call(Bitmap bitmap) {
                 FileOutputStream out = null;
                 try {
-                    out = new FileOutputStream("/data/data/com.tehmou.rxmaps/" + zoom + "_" + x + "_" + y + ".png");
+                    out = new FileOutputStream(getFilename(zoom, x, y));
                     bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
                 } catch (Exception e) {
                     e.printStackTrace();
